6ac291561d72f35d79fb23d93f9dca693a258ea9
laylalaisy <lsylayla@163.com>
2019-05-27 17:22:25 -0700

    update

---
+++ 6_basic_sum/basicsum.py
@@ -0,0 +1,31 @@
+## Spark Application - execute with spark-submit
+ 
+## Imports
+from __future__ import print_function
+import sys
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+ 
+## Module Constants
+APP_NAME = "My Spark Application"
+ 
+## Closure Functions
+ 
+## Main functionalitya
+ 
+def main(sc):
+    pass
+ 
+if __name__ == "__main__":
+   	# create an instance of a SparkSession as spark
+    spark = SparkSession.builder.appName("wordcount").getOrCreate()
+
+    # create SparkContext as sc
+    sc = spark.sparkContext
+
+    numbers = sc.parallelize([1, 2, 3, 4])
+    sum = numbers.fold(0, (lambda x, y: x+y))
+    print(sum)
+
+    # done!
+    spark.stop()
